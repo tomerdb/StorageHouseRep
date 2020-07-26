8b91bd4e67b525d807591333dc42fe4469bf01db
Tomer Harari <tomerdb@gmail.com>
2020-07-26 14:17:01 +0300

    Updates

--- precache-manifest.e283cb50d27484cd11aa079be4e34eec.js
+++ precache-manifest.e283cb50d27484cd11aa079be4e34eec.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "841420b2205bd339a8d50ec4561402ac",
+    "revision": "6546f219a017f7d2016bc6fe058615db",
     "url": "/StorageHouseRep/index.html"
   },
   {
-    "revision": "5d451727dae9158544a2",
+    "revision": "bd721867c4ef162697b8",
     "url": "/StorageHouseRep/static/css/2.829c9cb5.chunk.css"
   },
   {
-    "revision": "2b9d00d3ad662e39140e",
+    "revision": "d69ed59fe695fa6a87a7",
     "url": "/StorageHouseRep/static/css/main.6130e160.chunk.css"
   },
   {
-    "revision": "5d451727dae9158544a2",
-    "url": "/StorageHouseRep/static/js/2.f2936d3a.chunk.js"
+    "revision": "bd721867c4ef162697b8",
+    "url": "/StorageHouseRep/static/js/2.91ee008b.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/StorageHouseRep/static/js/2.f2936d3a.chunk.js.LICENSE.txt"
+    "url": "/StorageHouseRep/static/js/2.91ee008b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2b9d00d3ad662e39140e",
-    "url": "/StorageHouseRep/static/js/main.5234c136.chunk.js"
+    "revision": "d69ed59fe695fa6a87a7",
+    "url": "/StorageHouseRep/static/js/main.ac3e33c8.chunk.js"
   },
   {
     "revision": "02d08a725a3f49f457bd",
